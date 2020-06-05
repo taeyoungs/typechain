9612beb3ef447db39f93f07a1c483c35a8ef3044
taeyoung <xoxodudwkd@gmail.com>
2020-06-05 16:42:46 +0900

    Git Initialize
    Setting typescript config
    Create simple Block class

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number,
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+}
+
+const genesisBlock: Block = new Block(0, '1414141', '', 'blahblah', 12345);
+
+const blockChain: Block[] = [genesisBlock];
+
+console.log(blockChain);
+
+export {};
